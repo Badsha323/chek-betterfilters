18a3231a190cf0a9e7af705376fca002d49807bd
Badsha323 <98164078+Badsha323@users.noreply.github.com>
2022-05-02 14:29:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Badsha323/betterfilters.git /betterfilters
+  git clone https://github.com/AK4111/badsha.git /badsha
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /betterfilters
+  git clone $UPSTREAM_REPO /badsha
 fi
 cd /Ajax
 pip3 install -U -r requirements.txt
